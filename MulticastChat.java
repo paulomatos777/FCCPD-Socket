94a5c709bca2f17a6eb40a8e59de81e6125889b4
Paulo Matos <paulomatos4554@gmail.com>
2023-04-05 10:03:03 -0300

    update multicast

--- MulticastChat.java
+++ MulticastChat.java
@@ -1,94 +1,119 @@
-import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.MulticastSocket;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.io.IOException;
+import java.util.Scanner;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Scanner;
 
 public class MulticastChat {
 
     private static final String MULTICAST_ADDRESS = "239.0.0.1";
-    private static final int PORT = 1234;
-
-    public static void main(String[] args) {
-        try {
-            Scanner scanner = new Scanner(System.in);
-
-            System.out.print("Digite seu nome: ");
-            String userName = scanner.nextLine();
-
-            System.out.print("Digite o tópico de interesse (Entretenimento ou Esportes): ");
-            String topic = scanner.nextLine().toLowerCase();
-
-            MulticastSocket socket = new MulticastSocket(PORT);
-
-            InetAddress group = InetAddress.getByName(MULTICAST_ADDRESS);
-            socket.joinGroup(group);
-
-            Thread receiverThread = new Thread(new MessageReceiver(socket, userName, topic));
-            receiverThread.start();
-
-            while (true) {
-                System.out.print("Digite uma mensagem ou SAIR para sair do grupo: ");
-                String message = scanner.nextLine();
-
-                if (message.equalsIgnoreCase("SAIR")) {
-                    String leaveMessage = String.format("[%s] %s saiu do grupo.", getCurrentTime(), userName);
-                    byte[] leaveData = leaveMessage.getBytes();
-                    DatagramPacket leavePacket = new DatagramPacket(leaveData, leaveData.length, group, PORT);
-                    socket.send(leavePacket);
-
-                    socket.leaveGroup(group);
-                    socket.close();
-
-                    break;
-                }
-
-                String formattedMessage = String.format("[%s] %s : %s", getCurrentTime(), userName, message);
-                byte[] data = formattedMessage.getBytes();
-                DatagramPacket packet = new DatagramPacket(data, data.length, group, PORT);
-                socket.send(packet);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+    private static final int PORT1 = 1234;
+    private static final int PORT2 = 1235;
+    private static int PORT = 1;
 
     private static String getCurrentTime() {
-        return new SimpleDateFormat("dd/MM/yyyy - HH:mm").format(new Date());
+      return new SimpleDateFormat("dd/MM/yyyy - HH:mm").format(new Date());
     }
+    
+    public static void main(String[] args) throws IOException {
 
-    private static class MessageReceiver implements Runnable {
+        try (// Lê o nome e topico do usuário
+        Scanner scanner = new Scanner(System.in)) {
+          System.out.print("Digite seu nome: ");
+          String name = scanner.nextLine();
+          System.out.print("Digite o tópico: ");
+          String topic = scanner.nextLine();
 
-        private MulticastSocket socket;
-        private String userName;
-        private String topic;
+          if(topic.equals("Esportes") ){
+            PORT=PORT1;
+          }else{
+            PORT=PORT2;
+          }
 
-        public MessageReceiver(MulticastSocket socket, String userName, String topic) {
-            this.socket = socket;
-            this.userName = userName;
-            this.topic = topic;
-        }
+          // Cria um socket multicast
+          InetAddress group = InetAddress.getByName(MULTICAST_ADDRESS);
+          MulticastSocket socket = new MulticastSocket(PORT);
+   
 
-        @Override
-        public void run() {
-            try {
-                while (true) {
-                    byte[] buffer = new byte[1024];
-                    DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-                    socket.receive(packet);
+          try {
+              // Junta-se ao grupo multicast
+              socket.joinGroup(new InetSocketAddress(group, PORT), NetworkInterface.getByInetAddress(InetAddress.getLocalHost()));
 
-                    String message = new String(packet.getData(), 0, packet.getLength());
+            
+              // Cria uma thread para receber as mensagens do grupo multicast
+              Thread receiverThread = new Thread(() -> {
+                  try {
+                      // Define o tamanho máximo do pacote a ser recebido
+                      byte[] buffer = new byte[1024];
+                      DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
 
-                    if (message.toLowerCase().contains(topic) || message.contains(userName)) {
-                        System.out.println(message);
-                    }
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+                      while (true) {
+                          // Recebe um pacote do grupo multicast
+                          socket.receive(packet);
+
+                          // Exibe a mensagem recebida
+                          String message = new String(packet.getData(), 0, packet.getLength());
+                          String[] parts = message.split(":", 2);
+                          String text = parts[1];
+                          System.out.println(text);
+                      }
+                  } catch (IOException e) {
+                      e.printStackTrace();
+                  }
+              });
+              receiverThread.start();
+
+              
+
+              // Lê as mensagens a serem enviadas pelo usuário
+              System.out.println("Digite uma mensagem a ser enviada ou SAIR");
+              while (true) {
+                  System.out.print("> ");
+                  String text = scanner.nextLine();
+
+                  if (text.equalsIgnoreCase("SAIR")) {
+                      // Cria um pacote com a mensagem de saída do usuário
+                      SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy - HH:mm");
+                      String message = "[" + formatter.format(new Date()) + "] " + name + " saiu do grupo.";
+                      byte[] buffer = message.getBytes();
+                      DatagramPacket packet = new DatagramPacket(buffer, buffer.length, group, PORT);
+
+                      // Envia o pacote para o grupo multicast
+                      socket.send(packet);
+
+                      break; // Encerra o loop e encerra o programa
+                  } else {
+                      // Cria um pacote com a mensagem a ser enviada
+                      String formattedMessage = String.format("[%s] %s : %s", getCurrentTime(), name, text);
+                      byte[] data = formattedMessage.getBytes();
+                      DatagramPacket packet = new DatagramPacket(data, data.length, group, PORT);
+                      socket.send(packet);
+                      // socket.send(packet);
+                      
+                  }
+              }
+              
+          } catch (SocketException e) {
+              e.printStackTrace();
+          } catch (IOException e) {
+              e.printStackTrace();
+          } finally {
+              // Sai do grupo multicast
+              try {
+                  NetworkInterface netIf = NetworkInterface.getByInetAddress(InetAddress.getLocalHost());
+                  socket.leaveGroup(new InetSocketAddress(group, PORT), netIf);
+              } catch (IOException e) {
+                  e.printStackTrace();
+              }
+
+              // Fecha o socket
+              socket.close();
+          }
         }
     }
 }
